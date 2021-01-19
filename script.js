41ff575a063250b308561114a17f83d5a144c370
Manny Chacon <chmanuel2030@gmail.com>
2021-01-18 19:06:21 -0800

    functional and completed

--- script.js
+++ script.js
@@ -1,50 +1,46 @@
 // Assignment Code
-var password = document.querySelector('#generate');
-var smallL = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
-var bigL = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var character = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+"];
+var password = document.querySelector("#generate");
+password.addEventListener("click", generatePassword);
 
 
-function generatePassword() {
-    password.innerHTML = "";
-    var pwlength = (prompt("Please select a password length between 8 and 128 characters")); {
-    if(pwlength < 8) {
+function generatePassword(){
+    var pwlength = prompt("Please select a password length between 6 and 100 characters"); 
+    if(pwlength < 6) {
     prompt ("Select a larger number");
     }
-    else if (pwlength > 128) {
+    else if (pwlength > 100) {
       prompt("Select a smaller number");
     }
   
-    var character = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    var base = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
     var caps = confirm("Would you like capital letters in your password?");{
-        if(caps === true){character.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"); 
+        if(caps === true){base.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"); 
         alert("Capital letters will be in included");}
         else alert("Capital letters will be excluded");
-            }
+        console.log(base);
+    }
 
     var numbs = confirm("Would you like numbers in your password?");{
-        if(numbs === true){character.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+        if(numbs === true){base.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
         alert("Numbers will be included");}
-        else alert("Numbers will be excluded");            }
+        else alert("Numbers will be excluded");   
+        console.log(base);         
+    }
 
         var specials = confirm("Would you like special characters in your password?");{
-        if(specials === true){character.push("!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+");
+        if(specials === true){base.push("!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+");
         alert("Special characters will be included")}
         else alert("Special characters will be excluded")
-             }
-
-        for(var i = 0; i < length; i++){
-        
-        var inputs = character[Math.floor(Math.random() * character.length)]
-        document.getElementById("#password").innerHTML += inputs
-       
-        console.log(inputs)
+        console.log(base);
     }
- }
-} 
+    
+        for (var i = 0; i < pwlength; i++){
+        
+        var inputs = base[Math.floor(Math.random() * base.length)];
+        document.getElementById("password").innerHTML += inputs;
+    }
+}
 
 
-password.addEventListener('click', generatePassword);
 
